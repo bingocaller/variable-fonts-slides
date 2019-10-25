14aa26b8adb4e34a174b760ee35269d6c3879758
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2019-10-25 11:08:59 +0200

    Add deck, plus custom theme and layout

---
+++ theme.js
@@ -0,0 +1,15 @@
+export default {
+    fonts: {
+        body: '"Noto Sans", "Open Sans", Roboto, sans-serif',
+        monospace: '"FuraCode Nerd Font", "Roboto Mono", monospace',
+    },
+    fontWeights: {
+        body: 400,
+        heading: 500,
+    },
+    colors: {
+        text: '#535353',
+        background: '#fff',
+        primary: '#f26f61',
+    },
+}
